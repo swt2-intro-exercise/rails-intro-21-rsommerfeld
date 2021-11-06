6add96fc05bfd3ff10f7f6236b64aef6f97c78f6
rsommerfeld <romeo.sommerfeld@web.de>
2021-11-06 18:22:21 +0100

    Add failing test for #3

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -5,4 +5,10 @@ describe "New author page", type: :feature do
     # https://guides.rubyonrails.org/routing.html#path-and-url-helpers
     visit new_author_path
   end
+
+  it "should show a field with the name 'author[first_name]' on it" do
+    visit new_author_path
+
+    expect(page).to have_field('author[first_name]')
+  end
 end
