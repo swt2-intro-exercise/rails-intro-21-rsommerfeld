9c9add3a92d71d7a0765c45ab9f090e0eab55713
rsommerfeld <romeo.sommerfeld@web.de>
2021-11-06 18:24:23 +0100

    Extend failing test for #3

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -10,5 +10,6 @@ describe "New author page", type: :feature do
     visit new_author_path
 
     expect(page).to have_field('author[first_name]')
+    expect(page).to have_field('author[last_name]')
   end
 end
