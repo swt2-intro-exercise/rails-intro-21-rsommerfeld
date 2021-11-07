47b54ccfb2ca4affa99cb9ee9289b13ca11bc424
rsommerfeld <romeo.sommerfeld@web.de>
2021-11-07 21:05:57 +0100

    Add failing test for #7

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -18,4 +18,30 @@ describe "New author page", type: :feature do
     visit new_author_path
     expect(page).to have_css('input[type="submit"]')
   end
+
+  it "should not throw an error when submitting a filled-out form" do
+    # visit the new authors page, fill in the author info and submit
+    visit new_author_path
+    fill_in "author[first_name]", with: "Alan"
+    fill_in "author[last_name]", with: "Turing"
+    fill_in "author[homepage]", with: 'http://wikipedia.org/Alan_Turing'
+    find('input[type="submit"]').click
+  end
+
+  it "should actually create a new author when submitting the form" do
+    # expect to find a new author in the db
+    author = Author.where(first_name: "Alan", last_name: "Turing", homepage: "http://wikipedia.org/Alan_Turing").take
+    expect(author).to be_nil
+
+    # visit the new authors page, fill in the author info and submit
+    visit new_author_path
+    fill_in "author[first_name]", with: "Alan"
+    fill_in "author[last_name]", with: "Turing"
+    fill_in "author[homepage]", with: 'http://wikipedia.org/Alan_Turing'
+    find('input[type="submit"]').click
+
+    # expect to find a new author in the db
+    author = Author.where(first_name: "Alan", last_name: "Turing", homepage: "http://wikipedia.org/Alan_Turing").take
+    expect(author).not_to be_nil
+  end
 end
