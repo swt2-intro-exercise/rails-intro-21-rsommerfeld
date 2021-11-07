02b47bcb24b2361a873cd34e8d2deff94864cbb7
rsommerfeld <romeo.sommerfeld@web.de>
2021-11-07 21:28:37 +0100

    Allow saving new authors

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,18 @@
 class AuthorsController < ApplicationController
   def new
+    @author = Author.new
+  end
+
+  def create
+    @author = Author.new(author_params)
+    # save the author but always render the new page again
+    @author.save
+    render :new
+
+  end
+
+  private
+  def author_params
+    params.require(:author).permit(:first_name, :last_name, :homepage)
   end
 end
