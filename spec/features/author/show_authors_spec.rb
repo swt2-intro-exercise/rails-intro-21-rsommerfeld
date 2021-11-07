45ad239d7c7bcfc5a629b6216c49478171f65b06
rsommerfeld <romeo.sommerfeld@web.de>
2021-11-07 22:00:56 +0100

    Add failing test for #10

--- spec/features/author/show_authors_spec.rb
+++ spec/features/author/show_authors_spec.rb
@@ -4,5 +4,14 @@ describe "Show authors index page", type: :feature do
   it "should exist at 'authors_path' and render without an error" do
     visit authors_path
   end
+
+  it "should already contain information about a specific existing author" do
+    author = create :author
+    visit authors_path
+
+    expect(page).to have_text("Alan")
+    expect(page).to have_text("Turing")
+    expect(page).to have_text("http://wikipedia.de/Alan_Turing")
+  end
 end
 
