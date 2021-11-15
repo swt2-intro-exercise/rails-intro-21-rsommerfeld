a550a920b3fe4605efcb0dddfdfaebb12823d2fc
rsommerfeld <romeo.sommerfeld@web.de>
2021-11-15 17:19:51 +0100

    allow filtering papers by year on index page

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -3,7 +3,11 @@ class PapersController < ApplicationController
 
   # GET /papers
   def index
-    @papers = Paper.all
+    if params[:year] != nil
+      @papers = Paper.where(year: params[:year])
+    else
+      @papers = Paper.all
+    end
   end
 
   # GET /papers/1
--- spec/factories/papers.rb
+++ spec/factories/papers.rb
@@ -4,5 +4,10 @@ FactoryBot.define do
    venue { "Mind 49: 433-460" }
    year { 1950 }
    authors {build_list :author, 1}
+
+   factory :paper2 do
+     title { "Paper 2" }
+     year { 1968}
+   end
   end
 end
