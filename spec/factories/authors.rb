394bcddb5ab2192909b254cb0571d128272959a9
rsommerfeld <romeo.sommerfeld@web.de>
2021-11-15 15:56:12 +0100

    persist authors in db on edit or new for #24

--- spec/factories/authors.rb
+++ spec/factories/authors.rb
@@ -3,5 +3,11 @@ FactoryBot.define do
     first_name { "Alan" }
     last_name { "Turing" }
     homepage { "http://wikipedia.de/Alan_Turing" }
+
+    factory :author2 do
+      first_name { "Peter" }
+      last_name { "Plagiarist" }
+      homepage { "https//www.peter-plagiarist.de" }
+    end
   end
 end
