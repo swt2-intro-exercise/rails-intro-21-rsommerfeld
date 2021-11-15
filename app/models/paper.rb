8b93920826734214f1adfe62ae424b43268f085b
rsommerfeld <romeo.sommerfeld@web.de>
2021-11-15 12:56:22 +0100

    validate paper title existing for #19

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,2 +1,3 @@
 class Paper < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 1 }
 end
--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should have a constructor that can create paper instances" do
+    paper = Paper.new
+    expect(paper).to be_an_instance_of(Paper)
+  end
+
+  it "should should correctly set attributes during instance creation" do
+    title = "COMPUTING MACHINERY AND INTELLIGENCE"
+    venue = "Mind 49: 433-460"
+    year = 1950
+    paper = Paper.new(title: title, venue: venue, year: year)
+
+    expect(paper.title).to eq(title)
+    expect(paper.venue).to eq(venue)
+    expect(paper.year).to eq(year)
+  end
+
+  it "should not be valid without a title" do
+    paper = build :paper
+    paper.title = ""
+
+    expect(paper).to_not be_valid
+  end
 end
